6777637ef75d6bb4e33c019ccbeed3d8c570741a
Christian <christiantenz7@gmail.com>
2023-10-29 18:48:15 -0500

    nuevos proyectos añadidos

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-let toggle = document.getElementById('toggle');
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Previene el envío por defecto del formulario
 
-toggle.addEventListener('change', (e) => {
-    let checked = e.target.checked
-    document.header.classList.toggle('claro');
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('area').value;
 
-})
+        if (name === '' || email === '' || message === '') {
+            alert('Todos los campos son obligatorios');
+            return false;
+        } else {
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('area').value = '';
+        }
+
+    });
+});
+
+
+
+let cadena = 'gato';
+let cadenaReversa = cadena.split('').reverse().join();
+
+console.log(cadenaReversa)
